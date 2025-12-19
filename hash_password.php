4facba45c5a98b0b0eab4ee16a3018923a9f81f4
Omar Diab <omarkamaldiab9951@gmail.com>
2025-12-19 18:52:27 +0200

    Delete hash_password.php

--- hash_password.php
+++
@@ -1,9 +0,0 @@
-<?php
-$password = 'admin123';
-$hashed = password_hash($password, PASSWORD_DEFAULT);
-
-echo "<h2>Hashed Password Generator</h2>";
-echo "<p><strong>Original Password:</strong> admin123</p>";
-echo "<p><strong>Hashed Password:</strong></p>";
-echo "<textarea style='width:100%; height:100px;'>" . $hashed . "</textarea>";
-echo "<p>Copy the hashed password above and paste it in phpMyAdmin!</p>";
